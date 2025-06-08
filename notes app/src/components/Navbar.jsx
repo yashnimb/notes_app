4ccca9d5916aca6e3fc4016f50876ca121377148
Yash-N2003 <yash.nimb2003@gmail.com>
2025-06-08 16:37:01 +0530

    added tailwind

---
+++ notes app/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <img src="" alt="" />
+        <h1>NoteIt</h1>
+    </header>
+  )
+}
+
+export default Navbar
